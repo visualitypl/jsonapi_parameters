da0ae48163d212d00169a491d26072fc60aeefa3
marahin <me@marahin.pl>
2019-01-16 00:57:04 +0100

    Scaffold Posts and Authors with corresponding controllers

--- spec/app/db/schema.rb
+++ spec/app/db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_01_15_235549) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
 
 end
