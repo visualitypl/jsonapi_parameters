85ee690a4d41d1113a369adc56f4f727bff55808
marahin <me@marahin.pl>
2019-05-20 17:19:43 +0200

    Handle to-many relationship without included tree
    Included tree may or may not be passed (within a compound document),
    which means that it may occur that there are to-many relationships
    passed that are not present in the `included` tree.
    
    In such scenarios, we should make sure that relationship is translated
    in such a manner that it will yield something like:
    `genres_ids: [74]`.
    
    Related to #13 [issue](https://github.com/visualitypl/jsonapi_parameters/issues/13).

--- lib/jsonapi_parameters/version.rb
+++ lib/jsonapi_parameters/version.rb
@@ -1,5 +1,5 @@
 module JsonApi
   module Parameters
-    VERSION = '0.2.0'.freeze
+    VERSION = '0.3.0'.freeze
   end
 end
--- spec/support/inputs_outputs_pairs.rb
+++ spec/support/inputs_outputs_pairs.rb
@@ -11,90 +11,90 @@ module JsonApi::Parameters::Testing
       ],
       'single root, single relationship, multiple related resources' => [
         {
-         :data => {
-           :type => "contract",
-           :attributes => {
-             :total_amount=>"40.0",
-             :start_date=>"2018-10-18"
-           },
-           :relationships=>{
-             :products=>{
-               :data=>[
-                 {
-                   :id=>"4", :type=>"product"
-                 }, {
-                   :id=>"5", :type=>"product"
-                 }, {
-                   :id=>"6", :type=>"product"
-                 }
-               ]
-             },
-             :customer=>{
-               :data=>{
-                 :id=>"1", :type=>"customer"
-               }
-             }
-           }
-         },
-         :included=>[
-           {
-             :id=>"4",
-             :type=>"product",
-             :attributes=>{
-               :category=>"first_category",
-               :amount=>"15.0",
-               :name=>"First product",
-               :note=>""
-             }
-           },
-           {
-             :id=>"5",
-             :type=>"product",
-             :attributes=>{
-               :category=>"first_category",
-               :amount=>"10.0",
-               :name=>"Second Product",
-             }
-           },
-           {
-             :id=>"6",
-             :type=>"product",
-             :attributes=>{
-               :category=>"second_category",
-               :amount=>"15.0",
-               :name=>"Third Product",
-             }
-           }
-         ]
-       },
-       {
-         contract: {
-           start_date: '2018-10-18',
-           total_amount: '40.0',
-           customer_id: '1',
-           products_attributes: [
-             {
-               id: '4',
-               :category=>"first_category",
-               :amount=>"15.0",
-               :name=>"First product",
-               note: ''
-             },
-             {
-               id: '5',
-               :category=>"first_category",
-               :amount=>"10.0",
-               :name=>"Second Product"
-             },
-             {
-               id: '6',
-               :category=>"second_category",
-               :amount=>"15.0",
-               :name=>"Third Product"
-             }
-           ]
-         }
-       }
+          :data => {
+            :type => "contract",
+            :attributes => {
+              :total_amount => "40.0",
+              :start_date => "2018-10-18"
+            },
+            :relationships => {
+              :products => {
+                :data => [
+                  {
+                    :id => "4", :type => "product"
+                  }, {
+                    :id => "5", :type => "product"
+                  }, {
+                    :id => "6", :type => "product"
+                  }
+                ]
+              },
+              :customer => {
+                :data => {
+                  :id => "1", :type => "customer"
+                }
+              }
+            }
+          },
+          :included => [
+            {
+              :id => "4",
+              :type => "product",
+              :attributes => {
+                :category => "first_category",
+                :amount => "15.0",
+                :name => "First product",
+                :note => ""
+              }
+            },
+            {
+              :id => "5",
+              :type => "product",
+              :attributes => {
+                :category => "first_category",
+                :amount => "10.0",
+                :name => "Second Product",
+              }
+            },
+            {
+              :id => "6",
+              :type => "product",
+              :attributes => {
+                :category => "second_category",
+                :amount => "15.0",
+                :name => "Third Product",
+              }
+            }
+          ]
+        },
+        {
+          contract: {
+            start_date: '2018-10-18',
+            total_amount: '40.0',
+            customer_id: '1',
+            products_attributes: [
+              {
+                id: '4',
+                :category => "first_category",
+                :amount => "15.0",
+                :name => "First product",
+                note: ''
+              },
+              {
+                id: '5',
+                :category => "first_category",
+                :amount => "10.0",
+                :name => "Second Product"
+              },
+              {
+                id: '6',
+                :category => "second_category",
+                :amount => "15.0",
+                :name => "Third Product"
+              }
+            ]
+          }
+        }
       ],
       'https://jsonapi.org/format/#crud example' => [
         {
@@ -178,66 +178,107 @@ module JsonApi::Parameters::Testing
             ]
           }
         }
-      ]
-    ],
-    'PATCH update payloads' => [
-      'https://jsonapi.org/format/#crud example (modified, multiple photographers)' => [
+      ],
+      'relationships without included (issue #13 - https://github.com/visualitypl/jsonapi_parameters/issues/13)' => [
         {
           data: {
-            type: 'photos',
-            attributes: {
-              title: 'Ember Hamster',
-              src: 'http://example.com/images/productivity.png'
-            },
-            relationships: {
-              photographers: {
-                data: [
-                  {
-                    type: 'people',
-                    id: 9
-                  },
-                  {
-                    type: 'people',
-                    id: 10
+              type: 'movies',
+              attributes: {
+                title: 'The Terminator',
+                released_at: '1984-10-26',
+                runtime: 107,
+                content_rating: 'restricted',
+                storyline: 'A seemingly indestructible android is sent from 2029 to 1984 to assassinate a waitress, whose unborn son will lead humanity in a war against the machines, while a soldier from that war is sent to protect her at all costs.',
+                budget: 6400000
+              },
+              relationships: {
+                genres: {
+                  data: [
+                    {
+                      id: 74, type: 'genres'
+                    }
+                  ]
+                },
+                director: {
+                  data: {
+                    id: 682, type: 'directors'
                   }
-                ]
+                }
               }
             }
-          },
-          included: [
-            {
-              type: 'people',
-              id: 10,
-              attributes: {
-                name: 'Some guy'
-              }
-            },
-            {
-              type: 'people',
-              id: 9,
-              attributes: {
-                name: 'Some other guy'
-              }
-            }
-          ]
         },
         {
-          photo: {
-            title: 'Ember Hamster',
-            src: 'http://example.com/images/productivity.png',
-            photographers_attributes: [
-              {
-                id: 9,
-                name: 'Some other guy'
-              },
-              {
-                id: 10,
-                name: 'Some guy'
-              }
-            ]
+          movie: {
+            title: 'The Terminator',
+            released_at: '1984-10-26',
+            runtime: 107,
+            content_rating: 'restricted',
+            storyline: 'A seemingly indestructible android is sent from 2029 to 1984 to assassinate a waitress, whose unborn son will lead humanity in a war against the machines, while a soldier from that war is sent to protect her at all costs.',
+            budget: 6400000,
+            director_id: 682,
+            genres_ids: [74]
           }
         }
       ]
-    ]
+    ],
+    'PATCH update payloads' => [
+        'https://jsonapi.org/format/#crud example (modified, multiple photographers)' => [
+          {
+            data: {
+              type: 'photos',
+              attributes: {
+                title: 'Ember Hamster',
+                src: 'http://example.com/images/productivity.png'
+              },
+              relationships: {
+                photographers: {
+                  data: [
+                    {
+                      type: 'people',
+                      id: 9
+                    },
+                    {
+                      type: 'people',
+                      id: 10
+                    }
+                  ]
+                }
+              }
+            },
+            included: [
+              {
+                type: 'people',
+                id: 10,
+                attributes: {
+                  name: 'Some guy'
+                }
+              },
+              {
+                type: 'people',
+                id: 9,
+                attributes: {
+                  name: 'Some other guy'
+                }
+              }
+            ]
+          },
+          {
+            photo: {
+              title: 'Ember Hamster',
+              src: 'http://example.com/images/productivity.png',
+              photographers_attributes: [
+                {
+                  id: 9,
+                  name: 'Some other guy'
+                },
+                {
+                  id: 10,
+                  name: 'Some guy'
+                }
+              ]
+            }
+          }
+        ]
+      ]
   }
 end
