ccc68555adeba0e74be0591d533bb57d8a912494
marahin <me@marahin.pl>
2020-07-10 19:49:28 +0200

    Add extra dashed casing tests in reference to #40

---
+++ spec/integration/authors_dashed_controller_spec.rb
@@ -0,0 +1,88 @@
+require 'rails_helper'
+
+describe AuthorsDashedController, type: :controller do
+  describe 'create' do
+    it 'creates an author' do
+      params = {
+        data: {
+          type: 'authors',
+          attributes: {
+            name: 'John Doe'
+          }
+        }
+      }
+
+      post :create, params: params
+
+      expect(jsonapi_response).to eq(
+        data: {
+          id: '1',
+          type: 'author',
+          attributes: {
+            name: 'John Doe'
+          },
+          relationships: {
+            scissors: { data: nil },
+            posts: {
+              data: []
+            }
+          }
+        }
+      )
+    end
+
+    it 'creates an author with posts' do
+      params = {
+        data: {
+          type: 'authors',
+          attributes: {
+            name: 'John Doe'
+          },
+          relationships: {
+            posts: {
+              data: [
+                {
+                  id: '123',
+                  type: 'post'
+                }
+              ]
+            }
+          }
+        },
+        included: [
+          {
+            type: 'post',
+            id: '123',
+            attributes: {
+              title: 'Some title',
+              body: 'Some body that I used to love',
+              'category-name': 'Some category'
+            }
+          }
+        ]
+      }
+
+      post :create, params: params
+
+      expect(jsonapi_response[:data]).to eq(
+        id: '1',
+        type: 'author',
+        attributes: {
+          name: 'John Doe'
+        },
+        relationships: {
+          scissors: { data: nil },
+          posts: {
+            data: [
+              {
+                id: '1',
+                type: 'post'
+              }
+            ]
+          }
+        }
+      )
+      expect(Post.find(1).category_name).to eq('Some category')
+    end
+  end
+end
