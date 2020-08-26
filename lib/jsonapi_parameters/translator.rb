0d8015fac37736e4eb6dba33591b605b829497cf
marahin <me@marahin.pl>
2020-08-26 12:52:22 +0200

    Call Validator by full namespace

--- lib/jsonapi_parameters/translator.rb
+++ lib/jsonapi_parameters/translator.rb
@@ -18,12 +18,12 @@ module JsonApi::Parameters
 
     @jsonapi_unsafe_hash = ensure_naming(params, naming_convention)
 
-    Validator.new(@jsonapi_unsafe_hash.deep_dup).validate! if should_prevalidate?
+    JsonApi::Parameters::Validator.new(@jsonapi_unsafe_hash.deep_dup).validate! if should_prevalidate?
 
     formed_parameters
   rescue StandardError => err
     # Validate the payload and raise errors...
-    Validator.new(@jsonapi_unsafe_hash.deep_dup).validate! unless JsonApi::Parameters.suppress_validation_errors
+    JsonApi::Parameters::Validator.new(@jsonapi_unsafe_hash.deep_dup).validate! unless JsonApi::Parameters.suppress_validation_errors
 
     raise err # ... or if there were none, re-raise initial error
   end
