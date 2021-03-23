da8233ff4f9e4f9c48d12300ca1eb19ca5b866f2
Piotr Wasiak <chooosen@gmail.com>
2021-03-23 12:21:23 +0100

    Configure rails app to not serve assets and skip sprockets

--- spec/app/config/application.rb
+++ spec/app/config/application.rb
@@ -1,6 +1,9 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
 
 Bundler.require(*Rails.groups)
 require "jsonapi_parameters"
