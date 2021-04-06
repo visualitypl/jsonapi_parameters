14eabc31200788a244a590824eb2215d4b2c322d
Piotr Wasiak <chooosen@gmail.com>
2021-04-06 13:06:17 +0200

    Revert changes with session and flash. Document more about decision

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,27 +9,30 @@ def jsonapi_response
   JSON.parse(response.body, symbolize_names: true)
 end
 
-# rails changed test case request params from positional to keywords in rails 5.1.7
+# rails changed controller params from positional to keywords in rails 5.1.7
 # 5.1.6>= rails >=5.0 consumes both ways.
 # https://apidock.com/rails/v5.1.7/ActionController/TestCase/Behavior/process
 # others keywords can be: session, body, flash, format, xhr, as.
 # We are using `as: json` in specs (rails 5+),
 # but rails 4 does not require providing that and consumes only params, session and flash.
+# Integration specs does not have session and flash but we don't require them so we will skip them.
+# Integration reqests can pass also headers, but we don't use it yet.
+# https://github.com/rails/rails/blob/v6.0.3.6/actionpack/lib/action_dispatch/testing/integration.rb#L211
 # Please use this helper methods and use keyword arguments.
 # For get requests you should provide another helper integrating params, etc.
-def post_with_rails_fix(url, params: {}, session: nil, flash: nil, **others)
+def post_with_rails_fix(url, params: {}, **others)
   if Rails::VERSION::MAJOR >= 5
-    post url, params: params, session: session, flash: flash, **others
+    post url, params: params, **others
   else
-    post url, params, session, flash
+    post url, params, **others
   end
 end
 
 # Similar helper to post, providing compability to controller/request specs for rails 4
-def patch_with_rails_fix(url, params: {}, session: nil, flash: nil, **others)
+def patch_with_rails_fix(url, params: {}, **others)
   if Rails::VERSION::MAJOR >= 5
-    patch url, params: params, session: session, flash: flash, **others
+    patch url, params: params, **others
   else
-    patch url, params, session, flash
+    patch url, params, **others
   end
 end
